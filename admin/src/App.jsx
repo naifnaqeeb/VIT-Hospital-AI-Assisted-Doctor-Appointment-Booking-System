44db4c833509c6440a183357822b33a35a013a18
naifnaqeeb <naifnaqeeb.123@gmail.com>
2026-02-17 17:50:14 +0530

    Admin login api logic

---
+++ admin/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import Login from './pages/login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css' ;
+import { AdminContext } from './context/AdminContext';
+
+
+const App = () => {
+
+  const {aToken} = useContext(AdminContext)
+
+  return aToken ? (
+    <div>
+
+    <ToastContainer />
+    </div>
+  ) : (
+    <>
+    <Login />
+    <ToastContainer />
+    </>
+  )
+}
+
+export default App
