5e231da3bffae3c013be67468fa90fd6a5f6826d
naifnaqeeb <naifnaqeeb.123@gmail.com>
2026-03-01 18:33:45 +0530

    display booked appointments on frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -178,4 +178,18 @@ const bookAppointment = async (req,res) =>{
 
 }
 
-export {registerUser,loginUser,getProfile,updateProfile, bookAppointment}
+//API to get user appointments for frontend my-appointment page
+const listAppointment = async (req,res) => {
+    try {
+        
+        const {userId} = req.body
+        const appointments = await appointmentModel.find({userId})
+        res.json({success:true,appointments})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export {registerUser,loginUser,getProfile,updateProfile, bookAppointment, listAppointment}
